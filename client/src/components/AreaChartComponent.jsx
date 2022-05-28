fb0697043117a10f42b288ce0fe0053c11986dec
vaska <vaskania26@gmail.com>
2022-05-28 16:03:51 +0400

    fixed area chart

--- client/src/components/AreaChartComponent.jsx
+++ client/src/components/AreaChartComponent.jsx
@@ -1,15 +1,19 @@
-import {AreaChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer} from 'recharts'
-
+import {AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer} from 'recharts'
 
 const AreaChartComponent = ({data}) => {
   return (
       <ResponsiveContainer width='100%' height={300}>
-        <AreaChart data={data} margin={{top: 50}}>
+        <AreaChart
+            data={data}
+            margin={{
+              top: 50,
+            }}
+        >
           <CartesianGrid strokeDasharray='3 3'/>
           <XAxis dataKey='date'/>
-          <YAxis allowDataOverflow={false}/>
+          <YAxis allowDecimals={false}/>
           <Tooltip/>
-          <Bar type='monotone' dataKey='count' stroke='#2cb1bc' fill='#bef8fd'/>
+          <Area type='monotone' dataKey='count' stroke='#2cb1bc' fill='#bef8fd'/>
         </AreaChart>
       </ResponsiveContainer>
   )
