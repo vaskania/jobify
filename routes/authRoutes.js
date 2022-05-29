1dc9544743a41b9cd3e7aea3cf03478217e14a08
vaska <vaskania26@gmail.com>
2022-05-29 15:10:36 +0400

    added rate limit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,19 @@
 import express from "express";
+import rateLimiter from 'express-rate-limit'
 
 const router = express.Router()
 
-import { register, login, updateUser } from "../contollers/authControllers.js";
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  message: 'Too many request from this IP address, please try again after 15 minutes',
+})
+
+import {register, login, updateUser} from "../contollers/authControllers.js";
 import authenticateUser from '../middleware/auth.js'
 
-router.route('/register').post(register)
-router.route('/login').post(login)
+router.route('/register').post(apiLimiter, register)
+router.route('/login').post(apiLimiter, login)
 router.route('/updateUser').patch(authenticateUser, updateUser)
 
 export default router
