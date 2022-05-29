a86b14d7a368171fc2f0118b82772b205693e0e5
vaska <vaskania26@gmail.com>
2022-05-29 14:57:59 +0400

    added preparing for deployment, added script for build, changes server.js

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import 'express-async-errors'
+import morgan from 'morgan'
 
 const app = express()
 dotenv.config()
 
-import 'express-async-errors'
-import morgan from 'morgan'
+import {dirname} from 'path'
+import {fileURLToPath} from 'url'
+import path from 'path'
 
 //db and auth
 import connectDB from "./db/connect.js";
@@ -19,23 +22,25 @@ import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 import authenticateUser from './middleware/auth.js'
 
-if (process.env.NODE_ENV !== '!production') {
+if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'))
 }
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 app.use(express.json())
-
-const port = process.env.PORT || 5000
-
-app.get('/api/v1', (req, res) => {
-  res.json({ msg: 'API!' })
-})
-
+app.use(express.static(path.resolve(__dirname, './client/build')))
 app.use('/api/v1/auth', authRoutes)
 app.use('/api/v1/jobs', authenticateUser, jobsRoutes)
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+})
+
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 5000
 
 const start = async () => {
   try {
